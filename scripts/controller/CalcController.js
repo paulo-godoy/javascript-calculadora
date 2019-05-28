bd53958bb6cdd556807a73726dfe80389539cda3
unknown <paulo.ricardo@hammerconsult.com.br>
2019-05-28 15:21:23 -0300

    captura das teclas

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,6 +6,7 @@ class CalcController {
         this._timeEl =  document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
@@ -20,8 +21,24 @@ class CalcController {
     
     }
 
-    initButtonsEvents() {
-       let buttons = documents.querySelectorAll("#buttons > getComputedStyle, #parts > g"); 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    initButtonsEvents(){
+       let buttons = document.querySelectorAll("#buttons > g, #parts > g"); 
+       buttons.forEach((btn, index) =>{
+           this.addEventListenerAll(btn, "click drag", e=>{
+            console.log(btn.className.baseVal.replace("btn-",""));
+        });
+
+        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+            btn.style.cursor = "pointer";
+        });
+           
+       });
     }
 
     // carrega a data e hora da calculadora no carregamento da pagina.
