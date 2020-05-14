544f8c11b70cc0c03f2798aecdb3d5d8f36a83c5
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2020-05-14 12:16:33 -0300

    Iniando projeto calculadora

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,166 +1,20 @@
 class CalcController {
     constructor(){
-
-        this._operation = [];
-        this._locale = 'pt-br'; //referencia da data e hora
-        this._displayCalcEl =  document.querySelector("#display");
-        this._dateEl =  document.querySelector("#data");
-        this._timeEl =  document.querySelector("#hora");
-        this._currentDate;
+        
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
+        this._currentDate ;
         this.initialize();
-        this.initButtonsEvents();
     }
 
     initialize(){
-        //chama o metodo para carregar a data e hora.
-
-        this.setDisplayDateTime();
-
-       //Atualiza a data e hora
+        // Atualiza a data e hora a cada 1000 milisegundos
         setInterval(() => {
-            this.setDisplayDateTime();
+            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
         }, 1000);
-    
-    }
-
-    addEventListenerAll(element, events, fn){
-        events.split(' ').forEach(event => {
-            element.addEventListener(event, fn, false);
-        });
-    }
-
-    clearAll(){
-       this._operation = [];
-    }
-
-    clearEntry(){
-        this._operation.pop();
-    }
-
-    getLastOperation(){
-       return this._operation[this._operation.length - 1];
-    }
-
-    setLastOperation(value){
-        this._operation[this._operation.length - 1] = value;
-    }
-
-    isOperator(value) {
-        return (['+','-','*','%','/'].indexOf(value) > -1);
-    }
-
-    addOperation(value){
-        //ultimo valor digitado
-       if (isNaN(this.getLastOperation())) { 
-        //se for string cai aqui
-            if (this.isOperator(value)){
-                //trocar o operado
-                this.setLastOperation(value);
-            } else if(isNaN(value)) {
-                 //outra coisa
-                 console.log(value);
-            } else {
-                this._operation.push(value);
-            }
-       } else {
-        //se for numero cai aqui
-        let newValue = this.getLastOperation().toString() + value.toString();
-        this.setLastOperation(parseInt(newValue));
-       }
-       
-    }
-
-    setError(){
-       this.displayCalc = 'Error';
-    }
-
-    execBtn(value){
-        
-        switch (value) {
-            
-            case 'ac':
-                this.clearAll();
-            break;
-
-            case 'ce':
-                this.clearEntry();
-            break;
-
-            case 'soma':
-                this.addOperation('+');
-            break;
-
-            case 'subtracao':
-                this.addOperation('-');
-            break;
-
-            case 'divisao':
-                this.addOperation('*');
-            break;
-
-            case 'multiplicacao':
-                this.addOperation('%');
-            break;
-
-            case 'porcento':
-                this.addOperation('/');
-            break;
-
-            case 'igual':
-            
-            break;
-
-            case 'ponto':
-                this.addOperation('.');
-            break;
-
-            case '0':
-            case '1':
-            case '2':
-            case '3':
-            case '4':
-            case '5':
-            case '6':
-            case '7':
-            case '8':
-            case '9':
-                this.addOperation(parseInt(value));
-            break;
-
-            default:
-                this.setError();
-            break;
-
-            
-
-        }
-    }
-
-    initButtonsEvents(){
-       let buttons = document.querySelectorAll("#buttons > g, #parts > g"); 
-       buttons.forEach((btn, index) =>{
-           this.addEventListenerAll(btn, "click drag", e=>{
-           let textBtn = btn.className.baseVal.replace("btn-","");
-
-           this.execBtn(textBtn);
-        });
-
-        this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
-            btn.style.cursor = "pointer";
-        });
-           
-       });
-    }
-
-    // carrega a data e hora da calculadora no carregamento da pagina.
-    setDisplayDateTime() {
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
-            day: "2-digit",
-            month: "long",
-            year: "numeric"
-
-        });
-        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
     get displayTime(){
@@ -175,7 +29,7 @@ class CalcController {
         return this._dateEl.innerHTML;
     }
 
-    set displayDate(value) {
+    set displayDate(value){
         this._dateEl.innerHTML = value;
     }
 
@@ -191,7 +45,8 @@ class CalcController {
         return new Date();
     }
 
-    set dataAtual(value){
-        this._currentDate = value;
+    set currentDate(date){
+        this._currentDate = date;
     }
+
 }
