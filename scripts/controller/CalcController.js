2565bd2defbffa25ec6ab541dafa5fd0a615a12a
Paulo Ricardo Pinto Godoy <paulo.godoy@pampa.compasso>
2020-05-19 22:22:06 -0300

    for para armazenar o ultimo valor digitado

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -51,6 +51,7 @@ class CalcController {
         let last = this._operation.pop();
         let result = eval(this._operation.join(""));
         this._operation = [result, last];
+        this.setLastNumberToDisplay();
     }
 
     pushOperation(value) {
@@ -62,7 +63,16 @@ class CalcController {
     }
 
     setLastNumberToDisplay(){
+         let lastNumber;
+         for (let i = this._operation.length-1; i  >= 0; i-- ){
+            if(!this.isOperator(this._operation[i])) {
+                lastNumber = this._operation[i];
+                break;
+            }
+         }
         
+         this.displayCalc = lastNumber;
+
     }
 
     addOperation(value){
@@ -80,6 +90,7 @@ class CalcController {
             } else {
             
                 this.pushOperation(value);
+                this.setLastNumberToDisplay();
             
             }
 
